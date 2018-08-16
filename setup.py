039904cf402931a63b6163cf9dd5a67e64738a5c
Arif Dzikrullah <arifdzikrullah@Arifs-MacBook-Pro.local>
2018-08-16 19:41:22 +0700

    vv

--- setup.py
+++ setup.py
@@ -60,7 +60,11 @@ setup(
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['-e git+https://github.com/ardzix/django-datatable.git#egg=datatable'],
+    install_requires=[],
+    
+    dependency_links=[
+        "git+ssh://git@github.com/ardzix/django-datatable"
+    ],
 
     # List additional groups of dependencies here (e.g. development dependencies).
     # You can install these using the following syntax, for example:
