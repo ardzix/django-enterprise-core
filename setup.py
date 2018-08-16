10323b939e3577838a4d8976d25aa2b8da709170
Arif Dzikrullah <arifdzikrullah@Arifs-MacBook-Pro.local>
2018-08-16 20:07:37 +0700

    updated reqs

--- setup.py
+++ setup.py
@@ -60,10 +60,7 @@ setup(
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[
-        'Django==1.11.1',
-        'djangorestframework==3.6.3'
-    ],
+    install_requires=[],
 
     # List additional groups of dependencies here (e.g. development dependencies).
     # You can install these using the following syntax, for example:
