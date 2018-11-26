2411a5d6d8c2056f715193c60e61fc70b3c0243e
Arif Dzikrullah <arifdzikrullah@Arifs-MacBook-Pro.local>
2018-11-27 03:22:59 +0700

    modified init

---
+++ panel/structures/common/migrations/__init__.py
@@ -0,0 +1,19 @@
+'''
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# File: __init__.py
+# Project: django-panel-core
+# File Created: Tuesday, 27th November 2018 3:19:07 am
+# 
+# Author: Arif Dzikrullah
+#         ardzix@hotmail.com>
+#         https://github.com/ardzix/>
+# 
+# Last Modified: Tuesday, 27th November 2018 3:19:07 am
+# Modified By: arifdzikrullah (ardzix@hotmail.com>)
+# 
+# Hand-crafted & Made with Love
+# Copyright - 2018 Ardz Co, https://github.com/ardzix/django-panel-core
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+'''
+
+
