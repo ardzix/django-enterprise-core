990147e6f29759dad4ad48b1e02b7a2b2c011b16
Arif Dzikrullah <arifdzikrullah@Arifs-MacBook-Pro.local>
2019-04-05 16:50:17 +0700

    change user on ev oto to fk

--- panel/structures/authentication/models.py
+++ panel/structures/authentication/models.py
@@ -140,7 +140,7 @@ class EmailVerification(models.Model):
     email = models.EmailField()
     code = models.CharField(max_length=100)
     is_verified = models.BooleanField(default=False)
-    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
             return self.email
