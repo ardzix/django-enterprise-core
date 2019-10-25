26072d485487b6a1c549bc826032331f9f0c4941
Arif Dzikrullah <ardzyix@gmail.com>
2019-10-25 16:54:47 +0700

    updated gitignore on integration

---
+++ enterprise/structures/integration/migrations/__init__.py
@@ -0,0 +1,17 @@
+'''
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# File: __init__.py
+# Project: django-panel-core
+# File Created: Monday, 22nd April 2019 11:24:53 am
+#
+# Author: Arif Dzikrullah
+#         ardzix@hotmail.com>
+#         https://github.com/ardzix/>
+#
+# Last Modified: Monday, 22nd April 2019 11:24:54 am
+# Modified By: arifdzikrullah (ardzix@hotmail.com>)
+#
+# Hand-crafted & Made with Love
+# Copyright - 2018 Ardz Co, https://github.com/ardzix/django-panel-core
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+'''
