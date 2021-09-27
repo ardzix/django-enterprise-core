12be9d7e0925c546a7ae2538b02f8d6d518126f1
Fizi Kurniawan <fizikurniawan@gmail.com>
2021-09-27 13:38:14 +0700

    fix add mixins

---
+++ enterprise/libs/rest_module/viewsets/mixins.py
@@ -0,0 +1,30 @@
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.response import Response
+
+
+class ConstantMixin(GenericViewSet):
+    def generate_response(self):
+        constant = self.constant
+        response = [{"value": i[0], "text": i[1]} for i in constant]
+
+        return response
+
+    def list(self, request):
+        response = self.generate_response()
+
+        return Response(response)
+
+
+class ModelMixin(GenericViewSet):
+    value_text_field = {"value": "id62", "text": "display_name"}
+
+    def list(self, request):
+        response = [
+            {
+                "value": getattr(i, self.value_text_field.get("value")),
+                "text": getattr(i, self.value_text_field.get("text")),
+            }
+            for i in self.queryset
+        ]
+
+        return Response(response)
