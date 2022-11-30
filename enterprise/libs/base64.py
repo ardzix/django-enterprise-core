bf1377f6c9448fbbe47e1b4561a423bcba7f8fb2
Fizi Kurniawan <fizikurniawan@gmail.com>
2022-11-30 15:28:39 +0700

    support xlsx base64 upload

--- enterprise/libs/base64.py
+++ enterprise/libs/base64.py
@@ -6,6 +6,8 @@ from django.core.files.base import ContentFile
 def decode_base64(base64_text, file_name=None):
     format, imgstr = base64_text.split(";base64,")
     ext = format.split("/")[-1]
+    if ext == "vnd.ms-excel":
+        ext = "xlsx"
 
     if not file_name:
         file_name = str(uuid.uuid4())[:12]
